fc2745a209aaef115a11a89aebad0fc7263a7c1f
0xinit <28729137+0xinit@users.noreply.github.com>
2025-09-21 01:24:34 -0500

    added changes to vite configs

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import { resolve } from 'node:path'
 import { visualizer } from 'rollup-plugin-visualizer'
+import mkcert from 'vite-plugin-mkcert'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,12 +14,19 @@ export default defineConfig({
     TanStackRouterVite({ autoCodeSplitting: true }),
     viteReact(),
     tailwindcss(),
+    mkcert(),
     nodePolyfills({
       exclude: ['fs'],
       protocolImports: true,
     }),
     visualizer({ open: true, gzipSize: true, brotliSize: true }),
   ],
+  server: {
+    https: true,     // <-- serve over HTTPS
+    port: 3001,
+    host: 'localhost',
+  },
+  preview: { https: true },
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
